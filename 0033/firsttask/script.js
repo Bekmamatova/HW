e8ff23e442113449cb3dc1ac5a178e96b1756638
Bekmamatova <aidanekbekmamatova@gmail.com>
2022-02-09 22:37:25 +0600

    0033 is done

---
+++ 0033/firsttask/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elem = document.querySelector("div")
+    elem.innerText = 60
+    setInterval(() => {
+        let current = Number(elem.innerText)
+        current--
+        elem.innerText = current
+
+    }, 1000);
+
+
+})
